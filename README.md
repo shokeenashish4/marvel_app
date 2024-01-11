0674e4355ef24e284e8ad979f37c723ef2145ebc
Ashish Shokeen <shokeenashish4@gmail.com>
2024-01-11 16:01:42 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# marvel_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/app/marvel/marvel_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.marvel.marvel_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
